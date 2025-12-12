5999a7be89ee06a23fca9619ee7998029f4e700d
Bhone-Myat-Kyaw <bmkyawstudent14@gmail.com>
2025-12-12 16:07:33 +0630

    changing accordion text for memory section

--- frontend/src/pages/UserPages/Memory/Memory.tsx
+++ frontend/src/pages/UserPages/Memory/Memory.tsx
@@ -5,9 +5,9 @@ import { SelectedPage } from "../../../Shared/Types";
 import { SectionTitle } from "../../../Components/UserComponents";
 import { containerVariants } from "../../../Shared/framerVariants";
 // import img_1 from "/src/assets/img_1.png"
-import img_1 from "../../../assets/img_1.jpg"
+import img_1 from "../../../assets/img_4.jpg"
 import img_2 from "../../../assets/img_3.jpg"
-import img_3 from "../../../assets/img_4.jpg"
+import img_3 from "../../../assets/img_2.jpg"
 
 
 type Props = {
@@ -41,17 +41,17 @@ const Memory = ({setSelectedPage}: Props) => {
           </Accordion>
           <Accordion
             name={accordionGpName}
-            title="Gathering"
+            title="The Whole Fresher Welcome"
             imgPath={img_2}
           >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati, alias laboriosam dolorem pariatur assumenda vitae dolores blanditiis nisi? Deserunt obcaecati necessitatibus soluta, iure pariatur, asperiores rem perferendis porro fugiat, delectus ratione suscipit! Laudantium aut obcaecati distinctio beatae officiis debitis.
+            For YTU 12th batch, the whole fresher welcome was held in January 8, 2025. As the name suggests, it's a fresh welcome ceremony which was held by the combination of every major instead of a major-specific welcome. Back in our year, you can participate in singing or dance performances as you wish. Though we heard that there are some policy changes for 13th batch.
           </Accordion>
           <Accordion
             name={accordionGpName}
-            title="Gathering"
+            title="The Whole Farewell"
             imgPath={img_3}
           >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati, alias laboriosam dolorem pariatur assumenda vitae dolores blanditiis nisi? Deserunt obcaecati necessitatibus soluta, iure pariatur, asperiores rem perferendis porro fugiat, delectus ratione suscipit! Laudantium aut obcaecati distinctio beatae officiis debitis.
+            The whole farewell, or should i say "The first night party" in YTU history, was held in September 10, 2025. There are food stalls(you can sell snacks with your friends if you wish), singing and dancing performances. Unlike the whole welcome, there was a special event - designed for couples as most students believed. It is "Ballroom dance". But even if you are not in a relationship or don't have a dance partner, the students who organized the event will pair you up with someone who also don't have a partner ;)
           </Accordion>
         </motion.div>
       </motion.div>
